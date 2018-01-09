e7d22a5085e1db26713595507846008ac6647cdf
peeyuushh <peeyuushh@gmail.com>
2018-01-09 12:47:33 +0530

    getphonebook updated a bit

--- DataAccessLayer/GetPhoneBook.cs
+++ DataAccessLayer/GetPhoneBook.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DataAccessLayer
 {
     class GetPhoneBook
     {
-
+        public List<PhoneBook> GetDetails()
+        {
+            string phonebookCOnnectionstring="";
+            List<PhoneBook> contactList = new List<PhoneBook>();
+            SqlConnection getConnection = new SqlConnection(phonebookCOnnectionstring);
+            SqlCommand getCommand = new SqlCommand("spGetNameAndNumber", getConnection);
+            getCommand.CommandType = CommandType.StoredProcedure;
+            getCommand.Parameters.AddWithValue("@ID", 1);
+            SqlDataReader getReader = getCommand.ExecuteReader();
+            while(getReader.Read())
+            {
+                PhoneBook oneContact = new PhoneBook();
+                oneContact.fullName = getReader[0].ToString();
+                oneContact.phoneNumber = getReader[1].ToString();
+                contactList.Add(oneContact);
+            }
+            return contactList;
+        }
     }
 }
